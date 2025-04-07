3ca3402c46a4a3475505dfe1cba619bc0a145f9b
IuryGiovanny <iurygiovannygallhardogc@gmail.com>
2025-04-07 00:12:34 -0300

    ,,

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter  as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
-import "./App.css";
+import App from './App'
 import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <div>
-      <Header />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      <Footer />
-    </div>
+    <App />
   </StrictMode>,
 )
